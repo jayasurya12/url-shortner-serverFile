3f5c56e730f118faa9f5bb58513539ce4d43d8a2
jayasurya12 <jayasurya12vj@gmail.com>
2021-09-07 18:07:43 +0530

    nothing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.get("/first",(req,res)=>{
+app.get("/",(req,res)=>{
     res.json("it is working")
 })
 app.use("/",router)
